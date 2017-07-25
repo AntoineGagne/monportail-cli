af0a6a93a24334abe94365adeb7d363d76bdcf14
Antoine Gagné <gagnantoine@gmail.com>
2017-07-25 19:56:34 -0400

    Fix UI

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Exception ( bracket_
@@ -14,7 +16,12 @@ import System.IO ( hFlush
 import Control.Lens ( (^.) )
 
 import qualified Data.Aeson as Aeson
+import qualified Brick
+import qualified Brick.Main as BrickMain
 import qualified Data.ByteString as ByteString
+import qualified Data.Text as Text
+import qualified Data.Time as Time
+import qualified Data.Time.Clock as Clock
 import qualified Network.HTTP.Client as HttpClient
 import qualified Network.HTTP.Client.TLS as TlsHttpClient
 import qualified Network.Wreq as Wreq
@@ -22,6 +29,7 @@ import qualified Network.Wreq as Wreq
 import qualified Authentication
 import qualified Calendar
 import qualified Parser
+import qualified UI
 
 
 getUsername :: IO ByteString.ByteString
@@ -49,7 +57,21 @@ main = do
     manager <- HttpClient.newManager TlsHttpClient.tlsManagerSettings
     cookies <- Authentication.fetchAuthenticationCookies manager
     response <- Authentication.fetchCredentials user cookies
-    print response
+    currentTime <- Clock.getCurrentTime
+    timezone <- Time.getCurrentTimeZone
+    let localTime = Time.utcToLocalTime timezone currentTime
+        endingTime = (Time.utcToLocalTime timezone . Time.addUTCTime diffTime) currentTime
     case Authentication.parseLoginDetails (response ^. Wreq.responseBody) of
         Left errors -> print errors
-        Right loginDetails -> Calendar.fetchCalendarDetails manager loginDetails >>= print
+        Right loginDetails -> do
+            calendar <- Calendar.fetchCalendarDetails manager loginDetails
+            case calendar of
+              Left errors' -> print errors'
+              Right calendarDetails -> do
+                  events <- Calendar.fetchEvents manager loginDetails calendarDetails localTime endingTime
+                  case events of
+                      Left errors'' -> print errors''
+                      Right events' -> BrickMain.simpleMain (UI.displayEvents events' :: Brick.Widget Text.Text)
+    where
+        diffTime :: Time.NominalDiffTime
+        diffTime = 10368000
