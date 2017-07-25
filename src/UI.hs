de271b1f2205d918f529075263352f61165fb50d
Antoine Gagné <gagnantoine@gmail.com>
2017-07-25 19:56:34 -0400

    Fix UI

--- src/UI.hs
+++ src/UI.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module UI
     ( displayEvents
     ) where
@@ -6,6 +8,7 @@ import Brick.Widgets.Core ( (<+>)
                           , (<=>)
                           )
 import Control.Applicative ( liftA2 )
+import Data.Monoid ( (<>) )
 
 import qualified Brick
 import qualified Brick.Widgets.Core as Core
@@ -14,6 +17,7 @@ import qualified Brick.Widgets.Border.Style as BorderStyle
 import qualified Data.List as List
 import qualified Data.Map.Strict as Map
 import qualified Data.Maybe as Maybe
+import qualified Data.Text as Text
 import qualified Data.Time as Time
 
 import qualified Calendar
@@ -31,30 +35,29 @@ displayEvents events = let (eventsByStartingDate, eventsLongerThanOneDay) = sort
                            <=> buildEventsLongerThanOneDayWidget eventsLongerThanOneDay
     where normalEventsWidgets = Map.foldlWithKey' (\widget day events' -> widget <=> buildCompleteEventWidget day events') Core.emptyWidget
           buildCompleteEventWidget day events' = Core.withBorderStyle BorderStyle.unicode
-                                               $ Border.hBorderWithLabel (Core.str (' ' : show day ++ " "))
+                                               $ Border.hBorderWithLabel (Core.txt (" " <> (Text.pack . show) day <> " "))
                                               <=> Core.vBox (map buildEventWidget events')
-          buildEventsLongerThanOneDayWidget events' = Border.hBorderWithLabel (Core.str " Others ")
+          buildEventsLongerThanOneDayWidget events' = Border.hBorderWithLabel (Core.txt " Others ")
                                                    <=> Core.vBox (map buildEventLongerThanOneDayWidget events')
-          buildEventLongerThanOneDayWidget event' = Core.str ( (show . Maybe.fromJust . eventDate Calendar.startingDate) event' ++ " - " ++ (show . Maybe.fromJust . eventDate Calendar.endingDate) event')
+          buildEventLongerThanOneDayWidget event' = Core.txt ( (Text.pack . show . eventDate Calendar.startingDate) event' <> " – " <> (Text.pack . show . eventDate Calendar.endingDate) event')
                                                  <+> Core.txtWrap (Calendar.object event')
 
 buildEventWidget :: Calendar.Event -> Brick.Widget n
-buildEventWidget event = Core.str (formatEventTime event) <+> Core.txtWrap (Calendar.object event)
+buildEventWidget event = Core.txt (formatEventTime event) <+> Core.txtWrap (Calendar.object event)
     where
         formatEventTime event
             | Calendar.allDay event = "All day"
-            | otherwise = eventTime Calendar.startingDate event ++ " - " ++ eventTime Calendar.endingDate event
-        eventTime accessor event = show . Maybe.fromJust $ Time.localTimeOfDay . Calendar.fromULavalTime <$> accessor event
+            | otherwise = eventTime Calendar.startingDate event <> " – " <> eventTime Calendar.endingDate event
+        eventTime accessor event = Text.pack . show . Maybe.fromJust $ Time.localTimeOfDay . Calendar.fromULavalTime <$> accessor event
 
 sortEvents :: [Calendar.Event] -> (Map.Map Time.Day [Calendar.Event], [Calendar.Event])
 sortEvents events = (eventsByStartingDate, eventsLongerThanOneDay)
-    where eventsByStartingDate = List.foldl' (\m event -> Map.insertWith (++) (Maybe.fromJust . eventStartingDate $ event) (pure event) m) Map.empty filteredEvents
-          filteredEvents = List.sortOn (Maybe.fromJust . Calendar.startingDate) $ filter (compareEventDates' (==)) events
+    where eventsByStartingDate = List.foldl' (\m event -> Map.insertWith (++) (eventStartingDate event) (pure event) m) Map.empty filteredEvents
+          filteredEvents = List.sortOn Calendar.startingDate $ filter (compareEventDates (==)) events
           eventsLongerThanOneDay = filter (compareEventDates (/=)) events
-          compareEventDates compare event = not (Calendar.allDay event) && compareEventDates' compare event
-          compareEventDates' compare event = Maybe.fromMaybe False $ liftA2 compare (eventStartingDate event) (eventEndingDate event)
+          compareEventDates compare event = compare (eventStartingDate event) (eventEndingDate event)
           eventStartingDate = eventDate Calendar.startingDate
           eventEndingDate = eventDate Calendar.endingDate
 
-eventDate :: (Calendar.Event -> Maybe Calendar.ULavalTime) -> Calendar.Event -> Maybe Time.Day
-eventDate accessor event = Time.localDay . Calendar.fromULavalTime <$> accessor event
+eventDate :: (Calendar.Event -> Maybe Calendar.ULavalTime) -> Calendar.Event -> Time.Day
+eventDate accessor event = Maybe.fromJust $ Time.localDay . Calendar.fromULavalTime <$> accessor event
